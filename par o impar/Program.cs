21ef4d5fa4442a65e7e3f2dc8d74789e4532da8f
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-02-15 18:48:52 -0400

    Agregar archivos de proyecto.

---
+++ par o impar/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" par o impar");
+
+Console.WriteLine("Ingrese un número para determinar si es par o impar:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El número es par.");
+}
+else
+{
+    Console.WriteLine("El número es impar.");
+}
